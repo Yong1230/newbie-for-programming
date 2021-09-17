9d6864a80191753ebfb411290181d132841a3ad3
Yong1230 <jason112993@gmail.com>
2021-09-17 11:35:43 +0800

    Update 7-7-2 費氏數列.cpp

--- .vscode/7-7-2 費氏數列.cpp
+++ .vscode/7-7-2 費氏數列.cpp
@@ -1,2 +1,20 @@
 #include <iostream>
 using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int F[16];
+    F[0]=1;
+    F[1]=2;
+
+    for(int i=2; i<16; i++){
+        F[i]=F[i-1]+F[i-2];
+    }
+
+    int index;
+    cin>>index;
+    if(index>16 or index<0)    cout<<"error";
+    else    cout<<F[index];
+}
