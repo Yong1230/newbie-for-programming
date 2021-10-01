8e216c803c368874ca4c4024119d8ea55b1e24d9
hsieheting <yiting1109.hsieh@gmail.com>
2021-10-01 11:58:46 +0800

    選擇排序

--- .vscode/選擇排序.cpp
+++ .vscode/選擇排序.cpp
@@ -1,21 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    int arr[10];
+    int arr[10],big=0;
     srand(time(NULL));
     cout<<"before"<<endl;
     for(int i=0;i<10;i++){
         arr[i]=rand()%100+1;
         cout<<arr[i]<<endl;
     }
-    for(int i=9;i>=1;i--){
-        for(int j=0;j<i;j++){
-            if(arr[j]>arr[j+1]){
-                swap(arr[j],arr[j+1]);
+    cout<<"after"<<endl;
+    for(int i=9;i>0;i--){
+        big=0;
+        for(int j=0;j<=i;j++){
+            if(arr[big]<arr[j]){
+                big=j;
             }
         }
+        swap(arr[big],arr[i]);
     }
-    cout<<"after"<<endl;
     for(int i=0;i<10;i++){
         cout<<arr[i]<<endl;
     }
