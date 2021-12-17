6dfb891bbdf4b7107a606c01a2cbc3db4a37e9ac
hsieheting <yiting1109.hsieh@gmail.com>
2021-12-17 11:36:07 +0800

    12/17

--- .vscode/一元二次方程式.cpp
+++ .vscode/一元二次方程式.cpp
@@ -3,16 +3,15 @@ using namespace std;
 int main(){
     int a,b,c;
     cin>>a>>b>>c;
-    int d,ans1,ans2;
+    float d,ans1,ans2;
     d=b*b-4*a*c;
-    cout<<d<<endl;
     if(d>0){
-        ans1=(-1*b+sqrt(b*b-4*a*c))/(2*a);
-        ans2=(-1*b-sqrt(b*b-4*a*c))/(2*a);
+        ans1=((-b)+sqrt(d))/(2*a);
+        ans2=((-b)-sqrt(d))/(2*a);
         cout<<"x1="<<ans1<<", x2="<<ans2<<endl;
     }
     if(d==0){
-        ans1=(-1*b+sqrt(b*b-4*a*c))/(2*a);
+        ans1=(-1*b+sqrt(d))/(2*a);
         cout<<"x="<<ans1<<endl;
     }
     if(d<0){
